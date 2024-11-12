d182cdb518a92864e81367d3c06ae3ede3747b11
Flow <magebrnet@gmail.com>
2024-11-11 20:20:22 -0400

    Add files via upload

---
+++ exec11.py
@@ -0,0 +1,12 @@
+#CONTADOR DE PALAVRAS UNICAS EM UMA FRASE:
+
+frase = input('Digite uma frase: ')
+
+frase_unica = frase.split(" ")
+
+frase_unica = set(frase_unica)
+
+print('O total de palavras únicas nesta frase é: ', len(frase_unica))
+
+
+ 
