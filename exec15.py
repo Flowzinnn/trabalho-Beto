2b623df3c5d6dd06fd296109e21e045d203784b3
Flow <magebrnet@gmail.com>
2024-11-13 23:10:53 -0400

    Add files via upload

--- exec15.py
+++ exec15.py
@@ -3,3 +3,19 @@
 #O programa deve retornar: 1 -> se o número escolhido pelo programa é maior ou menor do qual ele chutou
 #2 - > O número de tentativas do jogador
 
+
+def adv_game():
+    from random import randint
+    attempts = 0
+    num_sort = randint(1, 100)
+    
+    while True:
+        int(input('Tente advinhar o número que eu estou pensando: '))
+    if attempt == num_sort:
+        return('Parabéns, você acertou o número!')
+
+adv_game()
+
+
+
+# https://chatgpt.com/share/67355db3-d7dc-800a-a2dd-2e6ff96ee753
